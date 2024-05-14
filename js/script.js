8d5bdf117e992879dcba5363754f4566dc757094
AdityaBheke <adityabheke34@gmail.com>
2024-05-14 16:05:48 +0530

    basic restructuring completed

--- js/script.js
+++ js/script.js
@@ -1,9 +1,5 @@
+import { ts, publicKey, hash } from "./credential.js";
 let favouriteCharacters = [];
-// Creating a Hash
-const ts = ((new Date()).getTime()).toString();
-const privateKey = 'e99b92619caa0536ef5b1dfe483652a7361c94cc';
-const publicKey = '013197631fe0d1dff2d0fad660597495';
-const hash = CryptoJS.MD5(ts+privateKey+publicKey).toString();
 
 // Navbar
 // Accessing navbar elements
