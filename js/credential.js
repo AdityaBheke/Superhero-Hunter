3fe3ca961d5d1d9ff2978efe304c2ac7d7002839
AdityaBheke <adityabheke34@gmail.com>
2024-05-14 13:43:19 +0530

    trying to revert again

--- js/credential.js
+++
@@ -1,7 +0,0 @@
-// Creating a Hash
-const ts = ((new Date()).getTime()).toString();
-const privateKey = 'e99b92619caa0536ef5b1dfe483652a7361c94cc';
-const publicKey = '013197631fe0d1dff2d0fad660597495';
-const hash = CryptoJS.MD5(ts+privateKey+publicKey).toString();
-
-export {ts,publicKey,hash};
--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// import md5 from 'crypto-js/md5.js';
+
 const ts = ((new Date()).getTime()).toString();
 const privateKey = 'e99b92619caa0536ef5b1dfe483652a7361c94cc';
 const publicKey = '013197631fe0d1dff2d0fad660597495';
