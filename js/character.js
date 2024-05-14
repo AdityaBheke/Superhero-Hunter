7feef35d4f7aa15b132b6ab8933992d26cbc7906
AdityaBheke <adityabheke34@gmail.com>
2024-05-15 02:39:21 +0530

    added comments in character.js

--- js/character.js
+++ js/character.js
@@ -1,11 +1,13 @@
 import { ts, publicKey, hash } from "./credential.js";  //importing all necessary credential from credential.js module
 import {heading, favPageButton} from './navbar.js';     //importing Page heading and FavouriteTab button with eventListeners attached to them
 
+// Accessing all necessary elements
 const profileImage = document.getElementById('superhero-profile');
 const profileName = document.querySelector('.search-label');
 const description = document.getElementById('description');
 const detailsContainer = document.getElementById('details-container'); //Main
 
+// function to get character details using Id as a parameter
 async function getCharacter(id) {
     const url = `https://gateway.marvel.com:443/v1/public/characters/${id}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
     try{
@@ -13,14 +15,15 @@ async function getCharacter(id) {
         const result = await response.json();
         const data = result.data;
         const character = data.results[0];
-        renderBasicDetails(character);
-        await getMoreDetails(character.comics.collectionURI,character.comics.available,'Comics');
-        await getMoreDetails(character.series.collectionURI,character.series.available,'Series');
-        await getMoreDetails(character.events.collectionURI,character.events.available,'Events');
+        renderBasicDetails(character);          //rendering basic details i.e. image, name and description of a superhero
+        await getMoreDetails(character.comics.collectionURI,character.comics.available,'Comics');   //Fetching Comics details of superhero
+        await getMoreDetails(character.series.collectionURI,character.series.available,'Series');   //Fetching Series details of superhero
+        await getMoreDetails(character.events.collectionURI,character.events.available,'Events');   //Fetching Events details of superhero
     }catch(error){
         console.log('error while fetching character ',error);
     }
 }
+// Function to get more details i.e. comics, series, events of a character using URI and available infos as parameter
 async function getMoreDetails(uri,total,infoType) {
     const url = `${uri}?ts=${ts}&apikey=${publicKey}&hash=${hash}`;
     try {
@@ -28,17 +31,18 @@ async function getMoreDetails(uri,total,infoType) {
         const result = await response.json();
         const data = result.data;
         const infoList = data.results;
-        renderMoreDetails(infoList,total,infoType);
+        renderMoreDetails(infoList,total,infoType);         //rendering more details i.e. comics, series, events of a character
     } catch (error) {
         console.log('error while fetching more details', error);
     }
 }
+// Function to render basic details i.e. image, name and description of a character
 function renderBasicDetails(character) {
     profileImage.src = character.thumbnail.path+'.'+character.thumbnail.extension;
     profileName.textContent = character.name;
     description.innerHTML = `&emsp;&emsp; ${character.description}`;
 }
-
+// Function to render more details i.e. comics, series, events of a character
 function renderMoreDetails(infoList,total,infoType) {
     const infoContainer = document.createElement('div');
     infoContainer.className = 'info-container';
